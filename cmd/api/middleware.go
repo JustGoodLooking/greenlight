d7ea66366e73a69415bba6f6f0d3519c87730f90
vm <lakingray@gmail.com>
2025-07-01 14:22:11 +0800

    authentication

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/tomasen/realip"
-	"golang.org/x/time/rate"
 	"net/http"
+	"strings"
 	"sync"
 	"time"
+
+	"github.com/tomasen/realip"
+	"golang.org/x/time/rate"
+	"greenlight.goodlooking.com/internal/data"
+	"greenlight.goodlooking.com/internal/validator"
 )
 
 func (app *application) recoverPanic(next http.Handler) http.Handler {
@@ -81,4 +86,54 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 
+}
+
+
+func (app *application) authenticate(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Authorization")
+
+		authorizationHeader := r.Header.Get("Authorization")
+
+		if authorizationHeader == "" {
+            r = app.contextSetUser(r, data.AnonymousUser)
+            next.ServeHTTP(w, r)
+            return
+        }
+
+		headerParts := strings.Split(authorizationHeader, " ")
+        if len(headerParts) != 2 || headerParts[0] != "Bearer" {
+            app.invalidAuthenticationTokenResponse(w, r)
+            return
+        }
+
+		token := headerParts[1]
+		v := validator.New()
+
+
+        if data.ValidateTokenPlaintext(v, token); !v.Valid() {
+            app.invalidAuthenticationTokenResponse(w, r)
+            return
+        }
+
+		user, err := app.models.Users.GetForToken(data.ScopeAuthentication, token)
+        if err != nil {
+            switch {
+            case errors.Is(err, data.ErrRecordNotFound):
+                app.invalidAuthenticationTokenResponse(w, r)
+            default:
+                app.serverErrorResponse(w, r, err)
+            }
+            return
+        }
+
+		r = app.contextSetUser(r, user)
+
+        // Call the next handler in the chain.
+        next.ServeHTTP(w, r)
+
+
+
+
+	})
 }
